d1abf8b388a11d2b8d701c27735e6a76c945a970
ErnestoGalindo1139 <galindohernandezernesto@gmail.com>
2024-01-27 02:49:02 -0700

    Se agrego boton de agregar al carrito en cards de ProductosPage

--- src/DogsBrothersStore/components/ProductsCardCatalogo.jsx
+++ src/DogsBrothersStore/components/ProductsCardCatalogo.jsx
@@ -1,10 +1,11 @@
+import { ShoppingCartIcon } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 
 export const ProductsCardCatalogo = ({ producto }) => {
     const { id_producto, nombre_producto, precio_producto, url_producto, descripcion_producto } = producto;
 
     return (
-        <li className="w-64 text-center lg:w-auto cardCatalogo">
+        <li className="w-64 lg:w-full h-[35rem]">
             <div className="">
                 <div className="overflow-hidden rounded-md">
                     <img
@@ -13,18 +14,21 @@ export const ProductsCardCatalogo = ({ producto }) => {
                         className="group-hover:opacity-75 imgCatalogo"
                     />
                 </div>
-                <div className="mt-6 relative text-justify">
-                    <h3 className="mt-1 font-semibold text-gray-900 hover:text-blue-600">
-                    <Link to={`/producto/${id_producto}`}>
-                        {`${nombre_producto} / ${descripcion_producto}`}
-                    </Link>
-                    <a href="#" className="">
-                    </a>
+                <div className="mt-6">
+                    <h3 className="mt-1 font-semibold text-gray-900 hover:text-blue-600 text-justify">
+                        <Link to={`/producto/${id_producto}`}>
+                            {`${nombre_producto} / ${descripcion_producto}`}
+                        </Link>
                     </h3>
                     <p className="mt-3 text-blue-900 font-bold">${precio_producto}</p>
                 </div>
                 
                     <p className="mt-2 text-white bg-green-700 pl-2 pr-2 pt-0.5 pb-0.5 w-40 font-bold rounded-md">✔ Con Existencia</p>
+                    <button className="text-white font-bold rounded-md mt-2 flex">
+                        <ShoppingCartIcon className="text-white w-8 h-9 bg-blue-500 hover:bg-blue-700 p-1.5 rounded-s" /> 
+                        <p className="px-2 bg-gray-500 hover:bg-gray-600 my-auto p-1.5 rounded-e">Agregar</p>
+                    </button>
+                    
             </div>
             
         </li>
--- src/DogsBrothersStore/pages/ProductosPage.jsx
+++ src/DogsBrothersStore/pages/ProductosPage.jsx
@@ -4,7 +4,7 @@ import { ProductosCatalogo } from "../components/ProductosCatalogo"
 export const ProductosPage = () => {
     return (
         <>
-            <div className="productosSeccion flex flex-col lg:flex-row  xl:flex-row justify-between ">
+            <div className="productosSeccion flex flex-col lg:flex-row  xl:flex-row justify-between">
                 {/* <div className="categorias"></div> */}
                 <CategoriasProductos />
                 <ProductosCatalogo />
